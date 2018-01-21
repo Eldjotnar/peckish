907fe6a11305ea1cb1fb59554a0672a79a84d295
Andreas Raduege <radueg1@stolaf.edu>
2018-01-21 13:19:35 -0600

    adding prototype recipe selection screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 
-import IngredientPicker from './screens/IngredientPicker'
-import Test from './screens/Test'
+import IngredientPicker from './screens/IngredientPicker';
+import Test from './screens/Test';
+import RecipePicker from './screens/RecipePicker';
 
 const Navi = StackNavigator({
   IP: {screen: IngredientPicker},
+  RP: {screen: RecipePicker},
   TS: {screen: Test},
 });
 
---
+++ screens/RecipePicker.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { AppLoading, Font } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import {Image, StyleSheet, Text, View, Dimensions, StatusBar, TouchableOpacity, FlatList} from 'react-native';
+import {StackNavigator} from 'react-navigation'
+
+import { TopBar } from '../components/TopBar';
+import { RecipeCard } from '../components/RecipeCard';
+var {width, height} = Dimensions.get('window');
+
+export default class RecipePicker extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false,
+      data: [
+         {
+            "imagePath":"../components/macandcheese.jpg",
+            "title":"Macaroni and Cheese",
+            "source":"BudgetBytes",
+            "missing":"0",
+            "id":"000"
+         },
+         {
+            "imagePath":"require('../components/macandcheese.jpg')",
+            "title":"Macaroni and Cheese",
+            "source":"BudgetBytes",
+            "missing":"0",
+            "id":"001"
+         }
+      ],
+    };
+  }
+
+  componentWillMount() {
+    this._loadAssetsAsync();
+  }
+
+  _loadAssetsAsync = async () => {
+    await Font.loadAsync({
+      multicolore: require('../assets/fonts/Multicolore.otf'),
+      comfortaa: require('../assets/fonts/Comfortaa.ttf'),
+      comfortaaBold: require('../assets/fonts/Comfortaa-Bold.ttf'),
+    });
+    this.setState({ loaded: true });
+  }
+  _keyExtractor = (item, index) => item.id;
+
+  _renderItem = ({item}) => (
+    <RecipeCard
+      title={item.title}
+      imagePath={require("../assets/images/macandcheese.jpg")}
+      source={item.source}
+      missing={item.missing}
+    />
+  );
+
+  static navigationOptions = {
+    header: null
+  }
+  render(){
+    const { navigate } = this.props.navigation;
+    if(!this.state.loaded){
+      return (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      )
+    }
+    return (
+      <View style={styles.main}>
+        <StatusBar
+          barStyle="light-content"
+          translucent={false} />
+        <Image source={require('../assets/images/banner.png')} style={styles.backgroundImage} />
+        <TopBar title="browse recipes"
+          leftIcon="ios-menu"
+          leftAction={this._leftAction}
+          rightIcon="ios-search"
+          rightAction={this._rightAction} />
+        <FlatList
+          data={this.state.data}
+          renderItem={this._renderItem}
+          keyExtractor={this._keyExtractor}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: '#191e45',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '60%',
+  },
+})
