c2401512266ed27927d7b5c69ad3d6e46952a1e1
Valent, Hugo <valent1@stolaf.edu>
2018-01-28 16:25:49 -0600

    first step towards camera reading barcodes

--- screens/Test.js
+++ screens/Test.js
@@ -9,25 +9,56 @@ import {
   StatusBar
 } from 'react-native';
 
+import {BarCodeScanner, Permissions} from 'expo'
+
 export default class Test extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      cameraPermission: null,
+    }
   }
   static navigationOptions = {
     header: null
   }
+
+  async componentWillMount(){
+    const { status } = await Permissions.askAsync(Permissions.CAMERA)
+    this.setState({cameraPermission: status === 'allowed'})
+  }
+
+  _handleBarCode = ({type, data}) => {
+    console.log(`Barcode read of type ${type} and data content: ${scanned}`)
+  }
+
+
   render(){
     const { navigate } = this.props.navigation;
-    return (
-      <View style={styles.main}>
-        <StatusBar
-          barStyle="light-content"
-          translucent={false}
-        />
-        <Image source={require('../assets/images/banner.png')} style={styles.backgroundImage} />
-        <View></View>
-      </View>
-    );
+    const { cameraPermission } = this.state;
+
+    if(cameraPermission === null) {
+      return <Text>Requesting permission for camera</Text>
+    }
+    else if (cameraPermission == false) {
+      return <Text>Lack permission for camera</Text>
+    }
+    else{
+      return (
+        <View style={styles.main}>
+          <StatusBar
+            barStyle="light-content"
+            translucent={false}
+          />
+          <Image source={require('../assets/images/banner.png')} style={styles.backgroundImage} />
+          <View>
+              <BarCodeScanner
+                onBarCodeRead = {this._handleBarCode}
+                style={StyleSheet.absoluteFill}
+              />
+          </View>
+        </View>
+        );
+    }
   }
 }
 
